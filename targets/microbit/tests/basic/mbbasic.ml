80d52e40189ffb4fcbe8ea7617c70f514ee9b935
Adrien Simonnet <adrien.simonnet@hotmail.fr>
2022-04-30 15:23:09 +0200

    test

--- targets/microbit/tests/basic/mbbasic.ml
+++ targets/microbit/tests/basic/mbbasic.ml
@@ -1,6 +1,6 @@
 open Screen
 
-let _ =
+let _ = 
   print_string "OMicroB!";
   print_int 42;
 
@@ -10,10 +10,13 @@ let _ =
                [false;false;false;false;false];
                [true;true;true;true;true]];
   while(true) do
-    if(ButtonA.is_on ())
+  set_pixel 2 9 true;
+    if(true)
     then (set_pixel 0 3 true; set_pixel 0 4 false)
     else (set_pixel 0 3 false; set_pixel 0 4 true);
-    if(ButtonB.is_on ())
+    if(true)
     then (set_pixel 4 3 true; set_pixel 4 4 false)
     else (set_pixel 4 3 false; set_pixel 4 4 true)
+    
   done
+  
